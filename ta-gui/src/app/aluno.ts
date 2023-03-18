a23879e9276ad9f35d6c7b1a7a72d0a7a533197e
Amanda-Quirino <aqrs@cin.ufpe.br>
2023-03-18 02:05:53 -0300

    feat: readicionando o elemento 'login github' no cadastro de alunos

--- ta-gui/src/app/aluno.ts
+++ ta-gui/src/app/aluno.ts
@@ -2,4 +2,12 @@ export class Aluno {
   nome: string;
   cpf: string;
   email: string;
+  loginGit: string;
+
+  constructor() {
+    this.nome = "";
+    this.cpf = "";
+    this.email = "";
+    this.loginGit = "";
+  }
 }
