4490541d1f7c3d3b725b43da01fc5095377a1857
Amanda-Quirino <aqrs@cin.ufpe.br>
2023-03-18 01:35:56 -0300

    feat: Adicao do elemento 'Login Github' do formulario para cadastro de alunos

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -7,17 +7,19 @@ import { NgModule } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   aluno: Aluno = {nome: "", cpf: "", email: ""};
+   aluno: Aluno = {nome: "", cpf: "", email: "", loginGitHub: ""};
 }
 
 export class Aluno {
   nome: string;
   cpf: string;
   email: string;
+  loginGitHub: string;
 
-  constructor(inputname:string, inputcpf:string, inputemail:string) {
+  constructor(inputname:string, inputcpf:string, inputemail:string, inputGitHub:string) {
     this.nome = inputname;
     this.cpf = inputcpf;
     this.email = inputemail;
+    this.loginGitHub = inputGitHub;
   }
 }
